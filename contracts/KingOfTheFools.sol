d6426207cd5d53fdc5272efbf388b129a69746ac
Camila marques <mila_@MacBook-Pro-3.local>
2022-09-30 20:28:59 -0300

    unittest

--- contracts/KingOfTheFools.sol
+++ contracts/KingOfTheFools.sol
@@ -4,102 +4,133 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+/// @title King Of Fools - Homework for Interview matters
+/// @author Camila Marques
+/// @notice You can deposit ETH or USDC into a smart contract, as long as it's
+/// at least 1.5x more money than the previous person. If
+/// you do that, you become "King of the fools",
+/// and your money gets sent back to the previous person.
+/// mock tokens were create in order to test function behaviour.
+/// @custom:experimental This is an experimental contract.
 
 contract KingOfTheFools {
-    //byte32ETH =0xaaaebeba3810b1e6b70781f14b2d72c1cb89c0b2b320c43bb67ff79f562f5ff4
-    //byte32 USDC = 0xd6aca1be9729c13d677335161321649cccae6a591554772516700f986f942eaa
-    //address public _USDC = 0x41e1b3476FB2b5EB3dC22Ed90213d90538c42500;
-    //address public _ETH = 0x94e5eb5B67e41b285dC551B902f362fA2D394f37;
-    uint256 public totalBalance;
-    address[] public users;
 
+    /// byte32ETH = 0xaaaebeba3810b1e6b70781f14b2d72c1cb89c0b2b320c43bb67ff79f562f5ff4
+    /// byte32USDC = 0xd6aca1be9729c13d677335161321649cccae6a591554772516700f986f942eaa
+    /// Fake Tokens Mocks contract
+    /// #address USDC = 0x41e1b3476FB2b5EB3dC22Ed90213d90538c42500;
+    /// #address public _ETH = 0x94e5eb5B67e41b285dC551B902f362fA2D394f37;
+
+    uint256 public totalBalance;
+    address public owner;
     struct Deposit {
-        uint256 id;
         uint256 amount;
         address user;
         bytes32 symbol;
+        bool deposited;
     }
-
-    mapping(bytes32 => address) public tokens;
-    bytes32[] public tokenList;
-    address public owner;
-    //tracks how many tokens were send by each address
     Deposit[] public deposits;
+    mapping(bytes32 => address) public tokens;
+    mapping(address => Deposit) fundsWinner;
+
+    event NewDeposit(address from, bytes32 token, uint256 amount, bool deposited);
 
     event NewKingOfTheFools(
         address winner,
         bytes32 token,
         uint256 amount,
-        bool deposited,
-        uint256 transactionId
+        bool deposited
     );
-
-    event NewDeposit(
-        bytes32 token,
-        uint256 amount,
-        bool deposited,
-        uint256 transactionId
-    );
-
-    constructor() {
-        owner = msg.sender;
-    }
 
     modifier tokenAccepted(bytes32 symbol) {
         require(tokens[symbol] != address(0), "Token is not accepted");
         _;
     }
 
+    modifier onlyWinner(address winner){
+    
+    require(fundsWinner[msg.sender].deposited == true, "No rewards to this address" );
+    _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function addToken(bytes32 symbol, address tokenAddress) external {
         require(msg.sender == owner, "Only Owner of the Contract");
         tokens[symbol] = tokenAddress;
-        tokenList.push(symbol);
     }
 
-    function checkPreviousAmount() internal view returns (uint256){
-
-       uint256 prevAmount = deposits[deposits.length - 1].amount;
-       prevAmount = (prevAmount * 3)/2;
-       return prevAmount;
-    }
+    /// @notice Deposit function have a condition, 
+    /// if the amount is > 1.5 * previousAmount than deposit in the contract,
+    /// otherwise should transfer the amount deposit to the previous address.
+    /// @dev function should accept only USDC and ETH (or anyother tokens that 
+    /// the owner add to the list by addToken function
 
     function deposit(uint256 _amount, bytes32 _symbol)
         external
-        tokenAccepted(_symbol) returns (bool){
-    
-        if(deposits.length == 0 || _amount > checkPreviousAmount()) 
-        {
-            IERC20(tokens[_symbol]).transferFrom(
-                msg.sender,
-                address(this),
-                _amount
-            );
-            deposits.push(
-                Deposit(deposits.length, _amount, msg.sender, _symbol)
-            );
+        tokenAccepted(_symbol)
+    {   require(_amount > 0, "should send more than zero tokens");
+        if (deposits.length == 0 || _amount > checkPreviousAmount()) {
+            
             totalBalance += _amount;
 
-            emit NewDeposit(_symbol, _amount, true, deposits.length);
-            return true;
+            depositToContract(msg.sender, _amount, _symbol);
 
+            emit NewDeposit(msg.sender, _symbol, _amount, true);
+          
         } else {
+
+            address winnerAddress = deposits[deposits.length - 1].user;
             
-            IERC20(tokens[_symbol]).transferFrom(
-                msg.sender,
-                deposits[deposits.length - 1].user,
-                _amount
-            );
-            deposits.push(
-                Deposit(deposits.length, _amount, msg.sender, _symbol)
-            );
+            depositToContract(msg.sender, _amount, _symbol);
+            
+            fundsWinner[winnerAddress] = deposits[deposits.length - 1];
+           
             emit NewKingOfTheFools(
-                deposits[deposits.length - 2].user,
+                winnerAddress,
                 _symbol,
                 _amount,
-                false,
-                deposits.length
+                false
             );
-            return false;
         }
     }
+
+    function withdrawReward() external onlyWinner(msg.sender) {
+        withdraw(msg.sender);
+    }
+
+    function withdraw(address _to) internal {
+
+        uint rewardAmount = fundsWinner[_to].amount;
+        bytes32 _symbol = fundsWinner[_to].symbol;
+        fundsWinner[_to].amount = 0;
+        IERC20(tokens[_symbol]).transfer(_to, rewardAmount);
+
+    }
+
+    function depositToContract(address from, uint256 _amount, bytes32 _symbol) internal  {
+
+        IERC20(tokens[_symbol]).transferFrom(
+                from,
+                address(this),
+                _amount
+        );
+
+        deposits.push(Deposit(_amount, from, _symbol, true));
+    }
+
+    /// @notice Returns the previous value deposited multipled for 1.5
+    /// @dev    Used fraction since solidity will not understand 1.5
+    /// @return previous price multiplied by 1.5
+
+    function checkPreviousAmount() internal view returns (uint256) {
+        uint256 prevAmount = deposits[deposits.length - 1].amount;
+        prevAmount = (prevAmount * 3) / 2;
+        return prevAmount;
+    }
+
 }
--- test/KingOfTheFools.js
+++ test/KingOfTheFools.js
@@ -1,12 +1,10 @@
-const {
-  loadFixture
-} = require("@nomicfoundation/hardhat-network-helpers");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
+const { assert, expect} = require("chai");
 const { ethers } = require("hardhat");
 
 describe("KingOfTheFools", function () {
- //fixtures useful for test the contract
+  //fixtures useful for test the contract
   async function deployKingOfTheFools() {
     // Getting accounts
     const [owner, user1, user2, user3] = await ethers.getSigners();
@@ -20,20 +18,19 @@ describe("KingOfTheFools", function () {
     const fakeUsdc = await FakeUsdc.deploy();
     const fool = await Fool.deploy();
 
-    await fakeEth.faucet(user1.address, 10000);
-    await fakeEth.connect(user1).approve(kingOfTheFools.address, 10000);
-    await fakeUsdc.faucet(user1.address, 10000);
-    await fakeUsdc.connect(user1).approve(kingOfTheFools.address, 10000);
-    await fool.faucet(user1.address, 10000);
+    await fakeEth.faucet(user1.address, 1000);
+    await fakeEth.connect(user1).approve(kingOfTheFools.address, 1000);
+    await fakeUsdc.faucet(user1.address, 1000);
+    await fakeUsdc.connect(user1).approve(kingOfTheFools.address, 1000);
+    await fool.faucet(user1.address, 1000);
 
-    await fakeEth.faucet(user2.address, 10000);
-    await fakeEth.connect(user2).approve(kingOfTheFools.address, 10000);
-    await fakeUsdc.faucet(user2.address, 10000);
-    await fakeUsdc.connect(user2).approve(kingOfTheFools.address, 10000);
+    await fakeEth.faucet(user2.address, 1000);
+    await fakeEth.connect(user2).approve(kingOfTheFools.address, 1000);
+    await fakeUsdc.faucet(user2.address, 1000);
+    await fakeUsdc.connect(user2).approve(kingOfTheFools.address, 1000);
 
-    await fakeEth.faucet(user3.address, 10000);
-    await fakeEth.connect(user3).approve(kingOfTheFools.address, 10000);
-
+    await fakeEth.faucet(user3.address, 1000);
+    await fakeEth.connect(user3).approve(kingOfTheFools.address, 1000);
 
     let ETH = ethers.utils.formatBytes32String("Eth");
     let USDC = ethers.utils.formatBytes32String("Usdc");
@@ -53,13 +50,11 @@ describe("KingOfTheFools", function () {
       ETH,
       USDC,
       FOOL,
-      fool
+      fool,
     };
   }
 
-
   describe("Deployment", function () {
-
     //**Happy Path */
     it("Should set the right owner", async function () {
       const { kingOfTheFools, owner } = await loadFixture(deployKingOfTheFools);
@@ -70,143 +65,204 @@ describe("KingOfTheFools", function () {
       const { fakeUsdc, fakeEth, user1, fool } = await loadFixture(
         deployKingOfTheFools
       );
-      expect(await fakeEth.balanceOf(user1.address)).to.equal(10000);
-      expect(await fakeUsdc.balanceOf(user1.address)).to.equal(10000);
-      expect(await fool.balanceOf(user1.address)).to.equal(10000);
+      expect(await fakeEth.balanceOf(user1.address)).to.equal(1000);
+      expect(await fakeUsdc.balanceOf(user1.address)).to.equal(1000);
+      expect(await fool.balanceOf(user1.address)).to.equal(1000);
     });
 
     it("should mint tokens to user2", async function () {
       const { fakeUsdc, fakeEth, user2, fool } = await loadFixture(
         deployKingOfTheFools
       );
-      expect(await fakeEth.balanceOf(user2.address)).to.equal(10000);
-      expect(await fakeUsdc.balanceOf(user2.address)).to.equal(10000);
-  
+      expect(await fakeEth.balanceOf(user2.address)).to.equal(1000);
+      expect(await fakeUsdc.balanceOf(user2.address)).to.equal(1000);
     });
 
     it("should add ETH on the contract", async function () {
-      const { kingOfTheFools, fakeEth, ETH} = await loadFixture(
+      const { kingOfTheFools, fakeEth, ETH } = await loadFixture(
         deployKingOfTheFools
       );
       expect(await kingOfTheFools.tokens(ETH)).to.equal(fakeEth.address);
     });
-    
+
     it("should add USDC on the contract", async function () {
       const { kingOfTheFools, fakeUsdc, USDC } = await loadFixture(
         deployKingOfTheFools
       );
       expect(await kingOfTheFools.tokens(USDC)).to.equal(fakeUsdc.address);
     });
-   
   });
-
-  //** Not Happy Path */
-
-  // it("Should set the right owner", async function () {
-  //   const { kingOfTheFools, user1 } = await loadFixture(deployKingOfTheFools);
-  //   expect(await kingOfTheFools.owner()).to.equal(user1.address);
-  // });
-
-  // it("should mint tokens to user1", async function () {
-  //   const { fakeUsdc, fakeEth, owner } = await loadFixture(
-  //     deployKingOfTheFools
-  //   );
-  //   expect(await fakeEth.balanceOf(owner.address)).to.equal(10000);
-  //   expect(await fakeUsdc.balanceOf(owner.address)).to.equal(10000);
-  // });
-
-  // it("should mint tokens to user2", async function () {
-  //   const { fakeUsdc, fakeEth, user2 } = await loadFixture(
-  //     deployKingOfTheFools
-  //   );
-  //   expect(await fakeEth.balanceOf(user2.address)).to.equal(100);
-  //   expect(await fakeUsdc.balanceOf(user2.address)).to.equal(0);
-  // });
-
-  // it("should add ETH on the contract", async function () {
-  //   const { kingOfTheFools, fakeEth, USDC} = await loadFixture(
-  //     deployKingOfTheFools
-  //   );
-  //   expect(await kingOfTheFools.tokens(USDC)).to.equal(fakeEth.address);
-  // });
-  
-  // it("should add USDC on the contract", async function () {
-  //   const { kingOfTheFools, fakeUsdc, ETH } = await loadFixture(
-  //     deployKingOfTheFools
-  //   );
-  //   expect(await kingOfTheFools.tokens(ETH)).to.equal(fakeUsdc.address);
-  // });
-
   describe("deposit", function () {
-
     it("should deposit ETH", async function () {
-      const { kingOfTheFools, fakeEth, ETH, user1, user2 } = await loadFixture(deployKingOfTheFools);
+      const { kingOfTheFools, fakeEth, ETH, user1, user2 } = await loadFixture(
+        deployKingOfTheFools
+      );
 
-      await kingOfTheFools.connect(user1).deposit(50,ETH);
+      await kingOfTheFools.connect(user1).deposit(50, ETH);
       expect(await fakeEth.balanceOf(kingOfTheFools.address)).to.equal(50);
       expect(await kingOfTheFools.totalBalance()).to.equal(50);
     });
-  
+
     it("should deposit USDC", async function () {
+      const { kingOfTheFools, fakeUsdc, USDC, user1 } =
+        await loadFixture(deployKingOfTheFools);
 
-      const { kingOfTheFools, fakeUsdc, USDC, user1, user2 } = await loadFixture(deployKingOfTheFools);
-
-      await kingOfTheFools.connect(user1).deposit(100,USDC);
+      await kingOfTheFools.connect(user1).deposit(100, USDC);
 
       expect(await fakeUsdc.balanceOf(kingOfTheFools.address)).to.equal(100);
       expect(await kingOfTheFools.totalBalance()).to.equal(100);
-
     });
 
-    it("should not deposit instead send token to user1", async function () {
+    it("should emit event NewDeposit", async function () {
+      const { kingOfTheFools, fakeUsdc, USDC, user1 } =
+        await loadFixture(deployKingOfTheFools);
 
-      const { kingOfTheFools, fakeUsdc, USDC, user1, user2 } = await loadFixture(deployKingOfTheFools);
+      expect(await kingOfTheFools.connect(user1).deposit(100, USDC)).to.emit(kingOfTheFools.address, "NewDeposit");
+     
+    });
 
-      await kingOfTheFools.connect(user1).deposit(100,USDC);
-      await kingOfTheFools.connect(user2).deposit(50,USDC);
+    it("should emit event NewKingOfTheFools", async function () {
+      const { kingOfTheFools, fakeUsdc, USDC, user1, user2 } =
+        await loadFixture(deployKingOfTheFools);
+
+      await kingOfTheFools.connect(user1).deposit(100, USDC);
+      expect(await kingOfTheFools.connect(user1).deposit(50, USDC)).to.emit(kingOfTheFools.address, "NewKingOfTheFools");
+      
+    });
+
+    it("should deposit USDC to user1 and user1 should withdraw it", async function () {
+      const { kingOfTheFools, fakeUsdc, USDC, user1, user2 } =
+        await loadFixture(deployKingOfTheFools);
+
+      await kingOfTheFools.connect(user1).deposit(100, USDC);
+      await kingOfTheFools.connect(user2).deposit(150, USDC);
+      await kingOfTheFools.connect(user1).withdrawReward();
 
       expect(await fakeUsdc.balanceOf(kingOfTheFools.address)).to.equal(100);
-      expect(await fakeUsdc.balanceOf(user1.address)).to.equal(9950);
-
+      expect(await fakeUsdc.balanceOf(user1.address)).to.equal(1050);
     });
 
-    it("should not deposit instead send token to user1", async function () {
+    it("should deposit ETH to user2", async function () {
+      const {
+        kingOfTheFools,
+        fakeUsdc,
+        USDC,
+        fakeEth,
+        ETH,
+        user1,
+        user2,
+        user3,
+      } = await loadFixture(deployKingOfTheFools);
 
-      const { kingOfTheFools, fakeUsdc, USDC, user1, user2 } = await loadFixture(deployKingOfTheFools);
-
-      await kingOfTheFools.connect(user1).deposit(100,USDC);
-      await kingOfTheFools.connect(user2).deposit(150,USDC);
-
-      expect(await fakeUsdc.balanceOf(kingOfTheFools.address)).to.equal(100);
-      expect(await fakeUsdc.balanceOf(user1.address)).to.equal(10050);
-
-    });
-
-    it("should deposit USDC", async function () {
-
-      const { kingOfTheFools, fakeUsdc, USDC, fakeEth, ETH, user1, user2, user3 } = await loadFixture(deployKingOfTheFools);
-
-      await kingOfTheFools.connect(user1).deposit(100,USDC);
-      await kingOfTheFools.connect(user2).deposit(151,USDC);
-      await kingOfTheFools.connect(user3).deposit(150,ETH);
+      await kingOfTheFools.connect(user1).deposit(100, USDC);
+      await kingOfTheFools.connect(user2).deposit(151, USDC);
+      await kingOfTheFools.connect(user3).deposit(150, ETH);
+      await kingOfTheFools.connect(user2).withdrawReward();
 
       expect(await fakeUsdc.balanceOf(kingOfTheFools.address)).to.equal(251);
       expect(await fakeEth.balanceOf(kingOfTheFools.address)).to.equal(0);
-      expect(await fakeEth.balanceOf(user2.address)).to.equal(10150);
+      expect(await fakeEth.balanceOf(user2.address)).to.equal(1150);
+    });
+  });
 
+  //** Not Happy Path */
+  describe("Deployment Not Happy Path", function () {
+    it("Should set the right owner", async function () {
+      const { kingOfTheFools, user1 } = await loadFixture(deployKingOfTheFools);
+      expect(await kingOfTheFools.owner()).to.not.equal(user1.address);
     });
 
+    it("should mint tokens to user1", async function () {
+      const { fakeUsdc, fakeEth, owner } = await loadFixture(
+        deployKingOfTheFools
+      );
+      expect(await fakeEth.balanceOf(owner.address)).to.not.equal(1030);
+      expect(await fakeUsdc.balanceOf(owner.address)).to.not.equal(1200);
+    });
 
-    // it("should fail if token is not accepted", async function () {
+    it("should mint tokens to user2", async function () {
+      const { fakeUsdc, fakeEth, user2 } = await loadFixture(
+        deployKingOfTheFools
+      );
+      expect(await fakeEth.balanceOf(user2.address)).to.not.equal(100);
+      expect(await fakeUsdc.balanceOf(user2.address)).to.not.equal(0);
+    });
 
-    //   const { kingOfTheFools, user1, user2, fool, FOOL } = await loadFixture(deployKingOfTheFools);
-    //   const notDeposit = await kingOfTheFools.connect(user1).deposit(100, FOOL);
-    //   expect(await notDeposit.to.be.revertedWithCustomError(kingOfTheFools, "Token is not accepted"));
-    //   // expect(await kingOfTheFools.connect(user2).deposit(100, FOOL)).revertedWithCustomError("Token is not accepted");
+    it("should add ETH on the contract", async function () {
+      const { kingOfTheFools, fakeEth, USDC } = await loadFixture(
+        deployKingOfTheFools
+      );
+      expect(await kingOfTheFools.tokens(USDC)).to.not.equal(fakeEth.address);
+    });
 
-    // });
+    it("should add USDC on the contract", async function () {
+      const { kingOfTheFools, fakeUsdc, ETH } = await loadFixture(
+        deployKingOfTheFools
+      );
+      expect(await kingOfTheFools.tokens(ETH)).to.not.equal(fakeUsdc.address);
+    });
   });
+
+  describe("Not Happy Path Deposit", function(){
+
+    it("should revert if token is not accepted", async function () {
+
+      const { kingOfTheFools, user1, user2, fool, FOOL } = await loadFixture(deployKingOfTheFools);
+      await expect(kingOfTheFools.connect(user1).deposit(1100,FOOL)).to.be.reverted;
+      
+    });
+
+    it("should revert if user haven't enough tokens", async function () {
+
+      const { kingOfTheFools, user1, ETH, fakeEth } = await loadFixture(deployKingOfTheFools);
+      await expect(kingOfTheFools.connect(user1).deposit(1100,ETH)).to.be.reverted;
+      
+    });
+
+    it("should revert if user send zero tokens", async function () {
+
+      const { kingOfTheFools, user1, ETH, fakeEth } = await loadFixture(deployKingOfTheFools);
+      await expect(kingOfTheFools.connect(user1).deposit(0,ETH)).to.be.revertedWith("should send more than zero tokens");
+      
+    });
+
+    it("should not allow user2 to withdrawReward", async function () {
+      const { kingOfTheFools, fakeEth, ETH, user1, user2 } = await loadFixture(
+        deployKingOfTheFools
+      );
+
+        await kingOfTheFools.connect(user1).deposit(10, ETH);
+        await kingOfTheFools.connect(user2).deposit(15, ETH);
+        await expect(kingOfTheFools.connect(user2).withdrawReward()).to.be.revertedWith("No rewards to this address");
+    });
+
+    it("should not allow user1 to withdrawReward", async function () {
+      const { kingOfTheFools, fakeEth, ETH, user1 } = await loadFixture(
+        deployKingOfTheFools
+      );
+      await kingOfTheFools.connect(user1).deposit(10, ETH);
+      await expect(kingOfTheFools.connect(user1).withdrawReward()).to.be.revertedWith("No rewards to this address");
+    
+    });
+
+     it("should not allow user2 to withdrawReward", async function () {
+      const { kingOfTheFools, fakeEth, ETH, user2 } = await loadFixture(
+        deployKingOfTheFools
+      );
+      await expect(kingOfTheFools.connect(user2).withdrawReward()).to.be.revertedWith("No rewards to this address");
   
+    });
 
+    it("should not allow user1 to withdrawReward", async function () {
+      const { kingOfTheFools, fakeEth, ETH, user2, user1, USDC } = await loadFixture(
+        deployKingOfTheFools
+      );
+        await kingOfTheFools.connect(user1).deposit(10, ETH);
+        await kingOfTheFools.connect(user2).deposit(16, USDC);
+        await expect(kingOfTheFools.connect(user1).withdrawReward()).to.be.revertedWith("No rewards to this address");
+  
+      });
+    
+    });
 
-});
+  });
